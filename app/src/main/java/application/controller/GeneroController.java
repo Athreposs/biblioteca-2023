cb90cf41efb559a152a710544b27baa0be652e81
Denis Tavares <denistvrs95@gmail.com>
2023-09-15 01:19:40 +0000

    ADD files

--- app/src/main/java/application/controller/GeneroController.java
+++ app/src/main/java/application/controller/GeneroController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import application.model.Genero;
 import application.model.GeneroRepository;
@@ -20,6 +20,11 @@ public class GeneroController{
     public String list(Model model){
         model.addAttribute("generos", generoRepo.findAll());
         return "/genero/list";
+    }
+
+    @RequestMapping("/insert")
+    public String insert() {
+        return "/genero/insert";
     }
 
     @RequestMapping(value = "/insert", method = RequestMethod.POST)
