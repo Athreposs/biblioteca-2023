e5c07ba3099a0554984e86e52a4e1d1a43552980
Denis Tavares <denistvrs95@gmail.com>
2023-09-15 01:14:33 +0000

    ADD files

--- app/src/main/java/application/model/genero.java
+++ app/src/main/java/application/model/genero.java
@@ -17,13 +17,13 @@ public class Genero {
     public int getId() {
         return id;
     }
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
-    public String getNome(){
+    public String getNome() {
         return nome;
     }
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 }
